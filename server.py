b7d76e3083cc32d8250b624a41f2e1eec3f43373
Paul Vergeev <paulvergeev@gmail.com>
2017-09-05 10:54:18 +0300

    Add input form

--- server.py
+++ server.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from flask import Flask, render_template, request
+from wtforms.fields import TextAreaField
+from wtforms import validators
+
 
 app = Flask(__name__)
+# this should be changed in production
+app.secret_key = 'change me'
 
-@app.route('/')
-def form():
-    return render_template('form.html')
+
+class SourceTextForm(FlaskForm):
+    source_text = TextAreaField(validators=[validators.required()])
+
+
+@app.route('/', methods=['GET', 'POST'])
+def typograph():
+    form = SourceTextForm(request.form)
+    if form.validate_on_submit():
+        raise ValueError(form.source_text)
+    return render_template('form.html', form=form)
+
 
 if __name__ == "__main__":
     app.run()

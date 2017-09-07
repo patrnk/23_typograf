1591e88995f61a0fa8ad862e9698434f884712e5
Paul Vergeev <paulvergeev@gmail.com>
2017-09-07 08:34:04 +0300

    Get rid of wtforms
    They were an overkill.

--- server.py
+++ server.py
@@ -1,33 +1,18 @@
-from flask_wtf import FlaskForm
 from flask import Flask, render_template, request
-from wtforms.fields import TextAreaField
-from wtforms import validators
-
 
 from utils import typograph_text
 
 
 app = Flask(__name__)
-# this should be changed in production
-app.secret_key = 'change me'
-
-
-class SourceTextForm(FlaskForm):
-    source_text = TextAreaField(validators=[validators.required()])
-
-
-class TypographedTextForm(FlaskForm):
-    typographed_text = TextAreaField()
 
 
 @app.route('/', methods=['GET', 'POST'])
 def typograph():
     context = {}
-    context['input_form'] = SourceTextForm(request.form)
-    context['output_form'] = TypographedTextForm()
-    if context['input_form'].validate_on_submit():
-        typographed_text = typograph_text(context['input_form'].source_text.data)
-        context['output_form'].typographed_text.data = typographed_text
+    context['source_text'] = request.form.get('text')
+    if context['source_text']:
+        typographed_text = typograph_text(context['source_text'])
+        context['typographed_text'] = typographed_text
     return render_template('form.html', **context)
 
 
